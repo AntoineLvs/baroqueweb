81a4647fda0784da45140b0e88887e383aaa564a
vagrant <vagrant@homestead>
2024-09-14 16:32:10 -0500

    update mario

--- resources/views/livewire/sidebar/app-sidebar.blade.php
+++ resources/views/livewire/sidebar/app-sidebar.blade.php
@@ -235,6 +235,21 @@
                                     Benutzer
                                 </a>
 
+                                    @if($super)
+                                    <a @if (request()->routeIs('tenants'))
+                                           class="bg-teal-700 text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md"
+                                       @endif
+
+                                       href="{{ route('tenant.list') }}"
+                                       class="text-gray-300 hover:bg-gray-700 hover:text-white group flex items-center px-2 py-2 text-sm font-medium rounded-md">
+                                        <!-- Heroicon name: outline/users -->
+                                        <svg class="text-gray-400 group-hover:text-gray-300 mr-3 flex-shrink-0 h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                                        </svg>
+                                        Tenants
+                                    </a>
+                                    @endif
+
                                 <hr class="mt-4 mb-4">
 
                                 <!-- <a @if (request()->routeIs('manufacturers.index'))
